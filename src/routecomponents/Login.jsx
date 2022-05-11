457f64801e26102d831dfabb6d31fe4f7e87105b
Ru Yi Shen <ruyishen2012@gmail.com>
2022-05-11 11:12:00 -0300

    fix : dependencies - status ongoing

--- src/routecomponents/Login.jsx
+++ src/routecomponents/Login.jsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+import styled from "styled-components";
+
 export default function Login() {
     return (
         <Div>
@@ -6,3 +9,24 @@ export default function Login() {
         </Div>
     )
 }
+
+
+/**************************** css ****************************/
+
+const Div = styled.div`
+
+    width: 100vw;
+    height: 100vh;
+    
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    color: var(--white-base);
+    font-size: 30px;
+
+    a {
+        text-decoration: underline;
+    }
+`;
--- src/routecomponents/Register.jsx
+++ src/routecomponents/Register.jsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+import styled from "styled-components";
+
 export default function Register() {
     return (
         <Div>
@@ -6,3 +9,24 @@ export default function Register() {
         </Div>
     )
 }
+
+
+/**************************** css ****************************/
+
+const Div = styled.div`
+
+    width: 100vw;
+    height: 100vh;
+    
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    color: var(--white-base);
+    font-size: 30px;
+
+    a {
+        text-decoration: underline;
+    }
+`;
