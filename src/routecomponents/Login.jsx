0d26ff39e897f8a7cd7a189dd3714d8e18722b48
Ru Yi Shen <ruyishen2012@gmail.com>
2022-05-12 11:00:36 -0300

    feat : user login request, store token in context and navigate to /userhome  - status ok

--- src/routecomponents/Login.jsx
+++ src/routecomponents/Login.jsx
@@ -1,32 +1,82 @@
-import { Link } from 'react-router-dom';
-import styled from "styled-components";
+import { useState, useEffect, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import UserContext from "../contexts/UserContext";
+
+import styled from 'styled-components';
+import Form from '../components/Form';
+import axios from 'axios';
+
 
 export default function Login() {
+    
+    const URL = "https://projeto14-driveneletro.herokuapp.com/signin";
+
+    const navigate = useNavigate();
+    const [userInfo, setUserInfo] = useState({});
+    const [disabled, setDisabled] = useState(false);
+    const { setUserData } = useContext(UserContext);
+
+    useEffect(() => {
+        /* if (localStorage.getItem('userData') !== null) {
+            setUserData(JSON.parse(localStorage.getItem('userData')));
+            navigate("/history");
+        } */
+        if (Object.keys(userInfo).length !== 0) {
+            setDisabled(true);
+            axios.post(URL, userInfo)
+                .then((response) => {
+                    //localStorage.setItem('userData', JSON.stringify(response.data));
+                    setUserData(response.data);
+                    navigate("/userhome");
+                })
+                .catch(error => {
+                    console.log(error);
+                    alert(error.response.data);
+                    setDisabled(false);
+                });
+        }
+    }, [userInfo, setUserData, navigate]);
+
     return (
-        <Div>
-            <h1>Página de Login</h1>
-            <Link to="/">Go Home</Link>
-        </Div>
+        <Main>
+            <h1>MyWallet</h1>
+            <Form type="login" setUserInfo={setUserInfo} disabled={disabled} />
+            <Link to="/register">Primeira vez? Cadastre-se!</Link>
+        </Main>
     )
 }
 
 
 /**************************** css ****************************/
 
-const Div = styled.div`
-
+const Main = styled.main`
     width: 100vw;
     height: 100vh;
-    
     display: flex;
     flex-direction: column;
     text-align: center;
-    justify-content: center;
     align-items: center;
-    color: var(--white-base);
-    font-size: 30px;
+    justify-content: center;
+    padding: 0 24px;
+    background-color: var(--purple-base);
+
+    h1 {
+        //font-family: 'Saira Stencil One';
+        font-weight: 700;
+        font-size: 32px;
+
+        color: var(--white-base);
+    }
+
+    form {
+        max-width: 430px;
+        margin: 32px 24px;
+    }
 
     a {
-        text-decoration: underline;
+        font-weight: 700;
+        font-size: 15px;
+
+        color: var(--white-base);
     }
-`;
+`
